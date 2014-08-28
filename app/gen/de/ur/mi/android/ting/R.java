4fc0016a4c4fc19294fd35320b2bba1d77c987fe
Riemi6 <maximilianriemer@gmx.de>
2014-08-28 11:32:03 +0200

    pin_layout

--- app/gen/de/ur/mi/android/ting/R.java
+++ app/gen/de/ur/mi/android/ting/R.java
@@ -861,10 +861,15 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070003;
+        public static final int abc_search_url_text_holo=0x7f070008;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
+        public static final int pin_button_background_color=0x7f070003;
+        public static final int pin_button_text_color=0x7f070004;
+        public static final int pin_content_text_color=0x7f070006;
+        public static final int pin_divider_background_color=0x7f070007;
+        public static final int pin_headline_text_color=0x7f070005;
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -992,6 +997,11 @@ containing a value of this type.
          */
         public static final int dialog_fixed_width_minor=0x7f080012;
         public static final int max_drawer_width=0x7f080017;
+        public static final int pin_content_margin=0x7f080019;
+        public static final int pin_content_text_size=0x7f08001b;
+        public static final int pin_divider_height=0x7f080018;
+        public static final int pin_healine_text_size=0x7f08001a;
+        public static final int pin_text_margin_top_bottom=0x7f08001c;
     }
     public static final class drawable {
         public static final int abc_ab_bottom_solid_dark_holo=0x7f020000;
@@ -1094,23 +1104,24 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_forgot_password=0x7f05004f;
+        public static final int action_forgot_password=0x7f050051;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05004e;
+        public static final int action_settings=0x7f050050;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button_like=0x7f05004c;
-        public static final int button_reting=0x7f05004d;
-        public static final int buttons=0x7f05004b;
+        public static final int button_like=0x7f05004e;
+        public static final int button_reting=0x7f05004f;
+        public static final int buttons=0x7f05004d;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f050044;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int div=0x7f050049;
         public static final int drawer_layout=0x7f050043;
         public static final int drawer_list=0x7f050045;
         public static final int dropdown=0x7f05000f;
@@ -1136,8 +1147,9 @@ containing a value of this type.
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int password=0x7f050040;
-        public static final int pin_content=0x7f05004a;
-        public static final int pin_headline=0x7f050049;
+        public static final int pin_content=0x7f05004c;
+        public static final int pin_divider=0x7f05004b;
+        public static final int pin_headline=0x7f05004a;
         public static final int pin_layout=0x7f050047;
         public static final int pin_picture=0x7f050048;
         public static final int progress_circular=0x7f050018;
@@ -1270,24 +1282,26 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_forgot_password=0x7f0a0017;
+        public static final int action_forgot_password=0x7f0a0019;
         public static final int action_settings=0x7f0a000f;
-        public static final int action_sign_in_register=0x7f0a0015;
-        public static final int action_sign_in_short=0x7f0a0016;
+        public static final int action_sign_in_register=0x7f0a0017;
+        public static final int action_sign_in_short=0x7f0a0018;
         public static final int app_name=0x7f0a000d;
+        public static final int button_like=0x7f0a0013;
+        public static final int button_reting=0x7f0a0012;
         public static final int drawer_close=0x7f0a0011;
         public static final int drawer_open=0x7f0a0010;
-        public static final int error_field_required=0x7f0a001c;
-        public static final int error_incorrect_password=0x7f0a001b;
-        public static final int error_invalid_email=0x7f0a0019;
-        public static final int error_invalid_password=0x7f0a001a;
+        public static final int error_field_required=0x7f0a001e;
+        public static final int error_incorrect_password=0x7f0a001d;
+        public static final int error_invalid_email=0x7f0a001b;
+        public static final int error_invalid_password=0x7f0a001c;
         public static final int hello_world=0x7f0a000e;
-        public static final int login_progress_signing_in=0x7f0a0018;
+        public static final int login_progress_signing_in=0x7f0a001a;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f0a0013;
-        public static final int prompt_password=0x7f0a0014;
-        public static final int title_activity_login=0x7f0a0012;
+        public static final int prompt_email=0x7f0a0015;
+        public static final int prompt_password=0x7f0a0016;
+        public static final int title_activity_login=0x7f0a0014;
     }
     public static final class style {
         /** 

3a45a072dafd6728d3285f470f16f30b8a7b8623
WoelliA <andreas.woelfl@stud.uni-regensburg.de>
2014-08-29 15:03:12 +0200

    fixed nullpointer

--- app/src/de/ur/mi/android/ting/app/activities/ActionBarActivityBase.java
+++ app/src/de/ur/mi/android/ting/app/activities/ActionBarActivityBase.java
@@ -1,10 +1,17 @@
 package de.ur.mi.android.ting.app.activities;
 
 import de.ur.mi.android.ting.app.IInjector;
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 public class ActionBarActivityBase extends ActionBarActivity implements IInjector {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.inject(this);
+	}
+	
 	@Override
 	public void inject(Object obj) {
 		((IInjector)this.getApplication()).inject(obj);
